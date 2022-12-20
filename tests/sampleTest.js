4fdac44a41c4d7b934bd18c8e66c757054a5585e
sowmyachan0107 <test.account@infobeans.com>
2022-12-20 17:40:22 +0530

    first commit

---
+++ tests/sampleTest.js
@@ -0,0 +1,12 @@
+describe("Learn Portal - Adaptive Practice Question test - Free user flow", async () => {
+    
+    it("smoke test 1", async () => {       
+               
+        await browser.url("https://google.com")
+        //await browser.maximizeWindow()  
+        await browser.pause(5000)
+       
+
+    })
+
+})
