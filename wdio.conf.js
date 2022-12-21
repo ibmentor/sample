c68c6496eeb3d9a756aafa4f022653262ae3c666
sowmyachan0107 <test.account@infobeans.com>
2022-12-21 11:11:53 +0530

    three

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,7 +23,7 @@ export const config = {
     // will be called from there.
     //
     specs: [
-        './test/specs/**/*.js'
+        'tests/sampleTest.js'
     ],
     // Patterns to exclude.
     exclude: [
