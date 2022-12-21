0dfaf38b1537ba1ee9563d0b06275f7e63de3555
sowmyachan0107 <test.account@infobeans.com>
2022-12-21 11:42:19 +0530

    five

--- tests/sampleTest.js
+++ tests/sampleTest.js
@@ -1,10 +1,12 @@
 describe("Learn Portal - Adaptive Practice Question test - Free user flow", async () => {
     
-    it("smoke test 1", async () => {       
-               
-        await browser.url("https://google.com")
+    it("smoke test 1", async () => { 
+   
+        let ans = 4        
+        expect(ans).toEqual(4)               
+        //await browser.url("https://google.com")
         //await browser.maximizeWindow()  
-        await browser.pause(5000)
+       // await browser.pause(5000)
        
 
     })
