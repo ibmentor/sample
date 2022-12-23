830c8b3547246906a8654f7c15bc02a5fb6caafb
ibmentor <43001921+ibmentor@users.noreply.github.com>
2022-12-23 17:08:11 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -59,8 +59,8 @@ export const config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
-         //'goog:chromeOptions': {
-            //args: ['--no-sandbox', '--headless']},
+         'goog:chromeOptions': {
+            args: ['--no-sandbox', '--headless']},
         //acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
