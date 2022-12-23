bb2f53d9481c8f8acb85c766c69b0a31f614c5f7
ibmentor <43001921+ibmentor@users.noreply.github.com>
2022-12-23 16:56:53 +0530

    Update sampleTest.js

--- tests/sampleTest.js
+++ tests/sampleTest.js
@@ -4,9 +4,9 @@ describe("Learn Portal - Adaptive Practice Question test - Free user flow", asyn
    
         let ans = 4        
         expect(ans).toEqual(4)               
-        //await browser.url("https://google.com")
-        //await browser.maximizeWindow()  
-       // await browser.pause(5000)
+        await browser.url("https://google.com")
+        await browser.maximizeWindow()  
+       await browser.pause(5000)
        
 
     })
